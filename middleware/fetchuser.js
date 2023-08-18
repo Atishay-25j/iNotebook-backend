3f31e70008d00b825c7544f434f4165cd90bf745
atishay_25 <codesome.aj.25@gmail.com>
2023-08-18 14:42:52 +0530

    llll

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -7,6 +7,8 @@ const fetchuser = (req,res, next)=>{
     const token = req.header('auth-token')
     console.log(token);
     if(!token){
+        console.log(token);
+        console.log(!token);
         res.status(401).send({error :"Please authenticate using a valid token not present"});
     }
     try {
