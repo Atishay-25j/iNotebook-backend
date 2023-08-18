9c10a852adfcad5b1bd49197fa756618f7afb47e
atishay_25 <codesome.aj.25@gmail.com>
2023-08-18 14:34:10 +0530

    kjk

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -5,6 +5,7 @@ const fetchuser = (req,res, next)=>{
     // Get the user from the jwt token and add id to the req object
 
     const token = req.header('auth-token')
+    console.log(token);
     if(!token){
         res.status(401).send({error :"Please authenticate using a valid token not present"});
     }
