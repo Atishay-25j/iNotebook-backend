66d1961806ae02136344cfbc13f81ec270366238
atishay_25 <codesome.aj.25@gmail.com>
2023-08-18 15:30:38 +0530

    llllllllllll

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -1,26 +1,28 @@
 const jwt = require('jsonwebtoken');
-const JWT_SECRET  = "AtishayIsaGoodBo$$y";
+const JWT_SECRET = "AtishayIsaGoodBo$$y";
 
-const fetchuser = (req,res, next)=>{
+const fetchuser = (req, res, next) => {
     // Get the user from the jwt token and add id to the req object
 
     const token = req.header('auth-token')
     console.log("token");
     console.log(token);
     console.log(!token);
-    if(!token){
+    if (!token) {
         console.log(token);
         console.log(!token);
-        res.status(401).send({error :"Please authenticate using a valid token not present"});
+        res.status(401).send({ error: "Please authenticate using a valid token not present" });
     }
     try {
         console.log("try");
-        const data = jwt.verify(token,JWT_SECRET);
-    req.user = data.user;
-    next()
+        const data = jwt.verify(token, JWT_SECRET);
+        console.log(data);
+        req.user = data.user;
+        console.log("Next");
+        next()
     } catch (error) {
-        res.status(401).send({error :"Please authenticate using a valid tokencatch"});
+        res.status(401).send({ error: "Please authenticate using a valid tokencatch" });
     }
-    
+
 }
 module.exports = fetchuser
