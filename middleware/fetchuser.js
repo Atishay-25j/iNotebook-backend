4ceb22bd1f263bc4e2da24e1b96033da3eec300b
atishay_25 <codesome.aj.25@gmail.com>
2023-08-18 14:29:27 +0530

    token

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -6,14 +6,14 @@ const fetchuser = (req,res, next)=>{
 
     const token = req.header('auth-token')
     if(!token){
-        res.status(401).send({error :"Please authenticate using a valid token"});
+        res.status(401).send({error :"Please authenticate using a valid token not present"});
     }
     try {
         const data = jwt.verify(token,JWT_SECRET);
     req.user = data.user;
     next()
     } catch (error) {
-        res.status(401).send({error :"Please authenticate using a valid token"});
+        res.status(401).send({error :"Please authenticate using a valid tokencatch"});
     }
     
 }
